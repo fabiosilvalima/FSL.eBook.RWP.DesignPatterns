0050b6f493e05debe4ed7a297a2fceba889d4e5c
Fabio Silva Lima <fabiosilvalima@terra.com.br>
2017-01-28 12:34:36 -0200

    service locator

---
+++ FSL.eBook.RWP.DesignPatterns/ServiceLocatorChapter/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using FSL.eBook.RWP.DesignPatterns.RepositoryChapter;
+using System.Web.Mvc;
+
+namespace FSL.eBook.RWP.DesignPatterns.ServiceLocatorChapter
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository ProductRepository
+        {
+            get
+            {
+                return DependencyResolver.Current.GetService<IProductRepository>();
+            }
+        }
+
+        public ActionResult Index()
+        {
+            var product = new Product()
+            {
+                Id = 1,
+                Name = "product 1"
+            };
+
+            ProductRepository.Insert(product);
+
+            return Content("Service Locator Pattern");
+        }
+    }
+}
---
+++ FSL.eBook.RWP.DesignPatterns/ServiceLocatorChapter/ProductHelper.cs
@@ -0,0 +1,24 @@
+﻿using FSL.eBook.RWP.DesignPatterns.RepositoryChapter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FSL.eBook.RWP.DesignPatterns.ServiceLocatorChapter
+{
+    public static class ProductHelper
+    {
+        public static IProductRepository ProductRepository()
+        {
+            var repository = DependencyResolver.Current.GetService<IProductRepository>();
+
+            if (repository == null)
+            {
+                repository = new DapperSqlProductRepository();
+            }
+
+            return repository;
+        }
+    }
+}
