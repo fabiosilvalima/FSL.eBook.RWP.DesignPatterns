72ceea54e25806916e339ab5b9a5a1e099c954f7
Fabio Silva Lima <fabiosilvalima@terra.com.br>
2017-01-30 06:49:55 -0200

    lazy loading pattern

---
+++ FSL.eBook.RWP.DesignPatterns/LazyLoadingChapter/ProductService.cs
@@ -0,0 +1,26 @@
+﻿using FSL.eBook.RWP.DesignPatterns.RepositoryChapter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FSL.eBook.RWP.DesignPatterns.LazyLoadingChapter
+{
+    public class ProductService
+    {
+        private IProductRepository _productRepository;
+        public IProductRepository ProductRepository
+        {
+            get
+            {
+                if (_productRepository == null)
+                {
+                    _productRepository = DependencyResolver.Current.GetService<IProductRepository>();
+                }
+
+                return _productRepository;
+            }
+        }
+    }
+}
